126a5ab96e095ffe7d8a5f5ae9d0b9e98c4f4d43
barcafatih <60362437+barcafatih@users.noreply.github.com>
2021-04-06 19:55:47 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 function myFunc(){
     var guessNum = document.getElementById("guessNumber").value;
     console.log(guessNum);
-    if(guessNum == Math.floor(10){
+    if(guessNum == Math.floor(10)){
         document.getElementById("aneG").innerText = "Tahminin doğru! Harikasın ❤️";
         document.getElementById("guessNumber").value ="";
         location.replace("https://github.com/barcafatih/rastgelesayi");
