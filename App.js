0cd659a7c65fc14aae116073a9caeb630f0c9cd1
barcafatih <60362437+barcafatih@users.noreply.github.com>
2021-04-06 19:39:15 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 var min = Math.ceil(0);
 var max = Math.floor(10);
-var actualNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
 var totalCorrect = 0;
 
 function myFunc(){
     var guessNum = document.getElementById("guessNumber").value;
     console.log(guessNum);
     console.log(actualNum);
-    if(actualNum == guessNum){
+    if(actualNum == Math.floor(Math.random() * (max - min + 1)) + min){
         document.getElementById("aneG").innerText = "Tahminin doğru! Harikasın ❤️";
         totalCorrect = totalCorrect + 1;
         document.getElementById("totalCorrect").innerText = totalCorrect;
