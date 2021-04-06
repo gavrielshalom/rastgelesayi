611dc584679328b7542f94a2e6374470df7d20e6
barcafatih <60362437+barcafatih@users.noreply.github.com>
2021-04-06 19:55:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,11 @@
 function myFunc(){
     var guessNum = document.getElementById("guessNumber").value;
     console.log(guessNum);
-    console.log(actualNum);
-    if(actualNum == Math.floor(10){
+    if(guessNum == Math.floor(10){
         document.getElementById("aneG").innerText = "Tahminin doğru! Harikasın ❤️";
+        document.getElementById("guessNumber").value ="";
         location.replace("https://github.com/barcafatih/rastgelesayi");
-
-        
+  
     }
     else{
         document.getElementById("aneG").innerText = "Tahminin maalesef yanlış. 😪";
